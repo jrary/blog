7e036d70bd44774dcaaade41cf7711b1d006c836
jrary <jrary@naver.com>
2024-04-11 10:45:23 +0900

    다시시작...

--- src/App.tsx
+++ src/App.tsx
@@ -1,162 +1,35 @@
-import { useRef } from 'react';
-import './App.css';
-import Hero from './stories/Hero/Hero';
-import Header from './stories/Header/Header';
-import HeroProfile from './stories/HeroProfile/HeroProfile';
-import AboutProfileText from './stories/AboutProfileText/AboutProfileText';
-import AboutProfileImage from './stories/AboutProfileImage/AboutProfileImage';
-import SocialMediaCarousel from './stories/SocialMediaCarousel/SocialMediaCarousel';
-import SkillGroupIcons from './stories/SkillGroupIcons/SkillGroupIcons';
-import ContactText from './stories/ContactText/ContactText';
-import FooterText from './stories/FooterText/FooterText';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { useTheme } from '../src/context/ThemeContext';
-
-const App = () => {
-  const { activeTheme } = useTheme();
-
-  const homeRef = useRef(null);
-  const aboutRef = useRef(null);
-  const scoialMediaRef = useRef(null);
-  const skillsRef = useRef(null);
-  const contactRef = useRef(null);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container">
-        <div>
-          <div className="hero-container" ref={homeRef}>
-            <Hero />
-            <div className="hero-header-container">
-              <Header
-                homeRef={homeRef}
-                aboutRef={aboutRef}
-                scoialMediaRef={scoialMediaRef}
-                skillsRef={skillsRef}
-                contactRef={contactRef}
-              />
-            </div>
-            <div className="hero-bio-container">
-              <HeroProfile />
-            </div>
-          </div>
-          <main>
-            <div ref={aboutRef}>
-              <section className="header-section">
-                <a
-                  href="/#about"
-                  rel="noopener noreferrer"
-                  className={
-                    activeTheme === 'light'
-                      ? 'header-title-light'
-                      : 'header-title-dark'
-                  }
-                >
-                  About
-                </a>
-              </section>
-              <section
-                className={`about-section ${
-                  activeTheme === 'light'
-                    ? 'section-border-light-theme'
-                    : 'section-border-dark-theme'
-                }`}
-              >
-                <div>
-                  <AboutProfileText />
-                </div>
-                <div>
-                  <AboutProfileImage />
-                </div>
-              </section>
-            </div>
-            <div ref={scoialMediaRef}>
-              <section className="header-section">
-                <a
-                  href="/#socialmedia"
-                  rel="noopener noreferrer"
-                  className={
-                    activeTheme === 'light'
-                      ? 'header-title-light'
-                      : 'header-title-dark'
-                  }
-                >
-                  Social Media
-                </a>
-              </section>
-              <section
-                className={`socialmedia-section ${
-                  activeTheme === 'light'
-                    ? 'section-border-light-theme'
-                    : 'section-border-dark-theme'
-                }`}
-              >
-                <SocialMediaCarousel />
-              </section>
-            </div>
-            <div ref={skillsRef}>
-              <section className="header-section">
-                <a
-                  href="/#skills"
-                  rel="noopener noreferrer"
-                  className={
-                    activeTheme === 'light'
-                      ? 'header-title-light'
-                      : 'header-title-dark'
-                  }
-                >
-                  Skills
-                </a>
-              </section>
-              <section
-                className={`skills-section ${
-                  activeTheme === 'light'
-                    ? 'section-border-light-theme'
-                    : 'section-border-dark-theme'
-                }`}
-              >
-                <SkillGroupIcons />
-              </section>
-            </div>
-            <div ref={contactRef}>
-              <section className="header-section">
-                <a
-                  href="/#contact"
-                  rel="noopener noreferrer"
-                  className={
-                    activeTheme === 'light'
-                      ? 'header-title-light'
-                      : 'header-title-dark'
-                  }
-                >
-                  Contact
-                </a>
-              </section>
-              <section
-                className={`contact-section ${
-                  activeTheme === 'light'
-                    ? 'section-border-light-theme'
-                    : 'section-border-dark-theme'
-                }`}
-              >
-                <ContactText />
-              </section>
-            </div>
-          </main>
-          {/* <footer className={activeTheme === 'light' ? 'footer-light-theme' : 'footer-dark-theme'}> */}
-          <footer
-            className={
-              activeTheme === 'light'
-                ? 'footer-light-theme'
-                : 'footer-light-theme'
-            }
-          >
-            <FooterText />
-          </footer>
-        </div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import * as path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import svgrPlugin from "vite-plugin-svgr";
-
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgrPlugin()],
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-});
+  plugins: [react()],
+})
